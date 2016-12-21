5afe0654b365081a82456c3d7223d6706f2bfa84
global77 <otss@ukr.net>
2016-12-21 13:47:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,43 +40,21 @@ int main(int argc, char **argv)
     pinMode (MS1, OUTPUT) ;
     pinMode (MS2, OUTPUT) ;
     pinMode (MS3, OUTPUT) ;
-/**
-    //motor(3,20,FULLST,true);
-    
-    //motor(2,1,QUATRST,true);
-    //motor(2,1,QUATRST,true);
-*/  
+
     int  tic = 1;
     bool dir = true;
     int sp = 20;
     while(true){ 
-       //motor(2,97,FULLST,true);
-       // motor(2,1,QUATRST,false);
-       // motor(2,1,SIXTHST,false);
-        //motor(2,1,FULLST,false);
-       // motor(2,1,QUATRST,false);
-        //motor(2,1,SIXTHST,true);
-         //motor(2,38,FULLST,false);  
-          //        motor(2,38,FULLST,false);  
-        
-        
         motor(sp,97/12,FULLST,STP1,dir);
-        //delay(5);
-        //if(tic==96 ){ tic=0; tic++;} else tic++;
-        
         if (tic%12 == 0)
         {
-          //  dir = !dir;
-            //sp --;
-            sp<3 ? sp=2 : sp--;
+           sp<3 ? sp=2 : sp--;
             cout << "speed = "<<sp<<endl;
         }
         tic == 960 ? tic=1 : tic++;
         
     }   
-      
-   // motor(5,1,HALFST,false);  
-    
+     
     return 0 ;
 }
 
